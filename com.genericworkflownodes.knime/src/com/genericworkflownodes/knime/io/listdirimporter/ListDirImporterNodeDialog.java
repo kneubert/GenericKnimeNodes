dc490572e4adb0131ede33f5ed5274b1e1811033
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-07-02 16:13:10 +0200

    correct ListDirImporter node

---
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/io/listdirimporter/ListDirImporterNodeDialog.java
@@ -0,0 +1,49 @@
+/**
+ * This file is part of GenericKnimeNodes.
+ * 
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.genericworkflownodes.knime.io.listdirimporter;
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
+
+/**
+ * <code>NodeDialog</code> for the "ListMimeFileImporter" Node.
+ * 
+ * 
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more
+ * complex dialog please derive directly from
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author roettig
+ */
+public class ListDirImporterNodeDialog extends DefaultNodeSettingsPane {
+
+    /**
+     * New pane for configuring ListMimeFileImporter node dialog. This is just a
+     * suggestion to demonstrate possible default dialog components.
+     * 
+     * @param obj
+     */
+    protected ListDirImporterNodeDialog() {
+        super();
+        addDialogComponent(new DialogComponentMultiDirChooser(
+                new SettingsModelStringArray(
+                        ListDirImporterNodeModel.CFG_DIRECTORYNAME,
+                        new String[] {})));
+ 
+    }
+}
---
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/io/listdirimporter/ListDirImporterNodeFactory.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2012, Marc Röttig.
+ *
+ * This file is part of GenericKnimeNodes.
+ * 
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.genericworkflownodes.knime.io.listdirimporter;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "ListDirImporter" Node.
+ * 
+ * 
+ * @author roettig
+ */
+public class ListDirImporterNodeFactory extends
+        NodeFactory<ListDirImporterNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ListDirImporterNodeModel createNodeModel() {
+        return new ListDirImporterNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<ListDirImporterNodeModel> createNodeView(
+            final int viewIndex, final ListDirImporterNodeModel nodeModel) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new ListDirImporterNodeDialog();
+    }
+
+}
